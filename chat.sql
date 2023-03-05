eb77c2ac59a5a8de63b7e165f573262e696522e6
selestinohajaniaina <selestinoolivier@gmail.com>
2023-03-05 13:38:37 +0300

    adding new table

--- chat.sql
+++ chat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 24 fév. 2023 à 14:24
+-- Généré le :  Dim 05 mars 2023 à 10:37
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -21,6 +21,59 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `chat`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id_coms` int(11) NOT NULL AUTO_INCREMENT,
+  `id_post` varchar(255) NOT NULL,
+  `idUser` varchar(255) DEFAULT NULL,
+  `texte` varchar(1000) DEFAULT NULL,
+  `date_coms` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_coms`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`id_coms`, `id_post`, `idUser`, `texte`, `date_coms`) VALUES
+(4, '19', '15', 'Bon logo pour ift', '2023-03-05 09:54:56'),
+(3, '19', '16', 'bonjour ift mahajanga', '2023-03-05 09:51:09'),
+(5, '19', '15', 'oui oui', '2023-03-05 09:57:15'),
+(6, '35', '15', 'good', '2023-03-05 13:34:32');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likepost`
+--
+
+DROP TABLE IF EXISTS `likepost`;
+CREATE TABLE IF NOT EXISTS `likepost` (
+  `idLike` int(11) NOT NULL AUTO_INCREMENT,
+  `id_post` varchar(255) NOT NULL,
+  `idUser` varchar(255) NOT NULL,
+  PRIMARY KEY (`idLike`)
+) ENGINE=MyISAM AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `likepost`
+--
+
+INSERT INTO `likepost` (`idLike`, `id_post`, `idUser`) VALUES
+(41, '19', '15'),
+(38, '19', '16'),
+(40, '16', '16'),
+(36, '22', '16'),
+(33, '20', '16'),
+(39, '14', '16'),
+(44, '35', '15');
 
 -- --------------------------------------------------------
 
@@ -90,15 +143,23 @@ CREATE TABLE IF NOT EXISTS `post` (
   `idOwner` varchar(10000) NOT NULL,
   `legend` varchar(1000) DEFAULT NULL,
   `photo` varchar(1000) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id_post`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `post`
 --
 
-INSERT INTO `post` (`id_post`, `idOwner`, `legend`, `photo`) VALUES
-(14, '16', 'c\'est ionic version 2', 'ehd4W.jpg');
+INSERT INTO `post` (`id_post`, `idOwner`, `legend`, `photo`, `date`) VALUES
+(14, '16', 'c\'est ionic version 2', 'ehd4W.jpg', '2023-02-25 11:45:20'),
+(20, '16', 'image depuis le www.orange.mg', 'PJZtq.jpg', '2023-02-25 11:45:20'),
+(16, '15', NULL, 'enH61.PNG', '2023-02-25 05:50:35'),
+(17, '15', 'Ma premiÃ¨re publication ðŸ¥°', 'BmPzt.jpg', '2023-02-25 09:29:48'),
+(19, '15', 'Institue de l\'IFT Mahajanga', 'W7TtQ.jpg', '2023-02-25 11:38:59'),
+(22, '16', 'An artist of considerable range, Jenna the name taken by Melbourne-raised, Brooklyn-based Nick Murphy writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.', '', '2023-03-01 22:36:04'),
+(33, '16', 'Hackathon 1er edition mahajanga', 'N203w.jpg', '2023-03-05 11:40:47'),
+(35, '16', 'petit plage', 'tiOom.jpg', '2023-03-05 11:43:26');
 
 -- --------------------------------------------------------
 
@@ -120,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`idUser`, `email`, `username`, `password`) VALUES
-(15, 'chauffemarcelhot@gmail.com', 'alexandre', '12345678'),
+(15, 'seha.karoka@gmail.com', 'alexandre', '12345678'),
 (16, 'selestinoolivier@gmail.com', 'selestino', '12345678');
 COMMIT;
 
